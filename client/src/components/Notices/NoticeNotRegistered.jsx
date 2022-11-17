f691ec5fa30c413b77df236dda98e346272a6eae
philippe <philippe.paulos@gmail.com>
2022-11-17 15:38:35 +0100

    refactor

---
+++ client/src/components/Notices/NoticeNotRegistered.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+function NoticeNotRegistered() {
+    return (
+      <Typography variant="p">
+        ⚠️ Your are not registered to this voting session.
+      </Typography>
+    );
+  }
+  
+  export default NoticeNotRegistered;
+  
---
+++ client/src/components/VotingApp.jsx
@@ -0,0 +1,25 @@
+
+import Voting from "../components/Voting";
+import Navbar from "../components/Navbar/index"
+import NoticeNotRegistered from "./Notices/NoticeNotRegistered"
+import VotingContext from "../contexts/VotingContext/VotingContext";
+import { useContext } from "react";
+
+function VotingApp() {
+    const { userSettings: { isRegistered } } = useContext(VotingContext);
+
+    return (
+
+        <>
+            {
+                !isRegistered ? <NoticeNotRegistered /> :
+                    <>
+                        <Navbar />
+                        <Voting />
+                    </>
+            }
+        </>
+    )
+}
+
+export default VotingApp;
